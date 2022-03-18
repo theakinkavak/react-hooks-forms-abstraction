6ce34c61749fef0bfc556777f47c6a4e64de17dc
Akin <akinkavak1@gmail.com>
2022-03-18 20:36:37 +0300

    Use checked property of checkboxes instead of value

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,27 +5,35 @@ function Form() {
   // const [lastName, setLastName] = useState("Henry");
   const [formData, setFormData] = useState({
     firstName: "John",
-    lastName: "Henry"
+    lastName: "Henry",
+    admin: false
   });
-
-  function handleFirstNameChange(event) {
-    setFormData({
-      ...formData,
-      firstName: event.target.value
-    });
-  }
   
-  function handleLastNameChange(event) {
+  function handleChange(event) {
+    const name = event.target.name;
+    let value = event.target.value;
+
+    if(event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+
     setFormData({
       ...formData,
-      lastName: event.target.value
+      [name]: value
     });
   }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log(formData);
+  }
+
       
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={formData.firstName} />
-      <input type="text" onChange={handleLastNameChange} value={formData.lastName} />
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="firstName" onChange={handleChange} value={formData.firstName} />
+      <input type="text" name="lastName" onChange={handleChange} value={formData.lastName} />
+      <input type="checkbox" name="admin" onChange={handleChange} value={formData.admin} />
       <button type="submit">Submit</button>
     </form>
   );
